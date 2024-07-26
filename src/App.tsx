b5cb764637afe07a1b0757305103e3563cf0331f
nigelbomett <nigelbomett@gmail.com>
2024-07-26 16:16:37 +0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Title } from '@mantine/core'
+
+function App() {
+
+  return (
+    <>
+      
+      <Title order={1}>This is h1 title</Title>
+      
+    </>
+  )
+}
+
+export default App
