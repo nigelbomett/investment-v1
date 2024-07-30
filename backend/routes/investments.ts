0ea842645b0e6b5a8ab4082652c788d19db9c084
Nigel Bomett <nigelbomett@gmail.com>
2024-07-30 12:26:58 +0300

    clean up

--- backend/routes/investments.ts
+++ backend/routes/investments.ts
@@ -3,9 +3,10 @@ import fs from 'fs-extra';
 import jwt from 'jsonwebtoken';
 import { Investment } from '../types';
 
+
 const router = express.Router();
-const dataPath = './data/investments.json';
-const secretKey = 'your_secret_key';
+const dataPath = './backend/data/investments.json';
+const secretKey = 'your_secret_key'; //TODO: add to .env
 
 const readData = async (): Promise<Investment[]> => {
     const data = await fs.readFile(dataPath, 'utf-8');
@@ -18,7 +19,9 @@ const writeData = async (data:Investment[]) => {
 
 
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
     if(!token) return res.sendStatus(401);
 
     jwt.verify(token,secretKey, (err,user) => {
@@ -36,6 +39,8 @@ router.get('/',authenticateToken, async(req:Request, res: Response) => {
 router.post('/',authenticateToken, async (req:Request,res:Response) => {
     const investments = await readData();
     const newInvestment = req.body;
+    if(!newInvestment.name) return res.status(400).json({error: `Please provide all the required details`});
+    if (!newInvestment.amount) return res.status(400).json({ error: `Please provide all the required details` });
     investments.push(newInvestment);
     await writeData(investments);
     res.status(201).json(newInvestment);
