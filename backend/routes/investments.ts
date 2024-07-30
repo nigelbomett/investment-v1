3e733ff8c1a7720a3e81c717f67d7eee0fa69d1d
Nigel Bomett <nigelbomett@gmail.com>
2024-07-30 09:55:57 +0300

    added investments route

---
+++ backend/routes/investments.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from 'express';
+import fs from 'fs-extra';
+import jwt from 'jsonwebtoken';
+import { Investment } from '../types';
+import { write } from 'fs';
+
+const router = express.Router();
+const dataPath = './data/investments.json';
+const secretKey = 'your_secret_key';
+
+const readData = async (): Promise<Investment[]> => {
+    const data = await fs.readFile(dataPath, 'utf-8');
+    return JSON.parse(data);
+};
+
+const writeData = async (data:Investment[]) => {
+    await fs.writeFile(dataPath,JSON.stringify(data,null,2));
+};
+
+const authenticateToken = (req: Request, res: Response, next: Function) => {
+    const token = req.headers['authorization'];
+    if(!token) return res.sendStatus(401);
+
+    jwt.verify(token,secretKey, (err,user) => {
+        if(err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+router.get('/',authenticateToken, async(req:Request, res: Response) => {
+    const investments = await readData();
+    res.json(investments);
+});
+
+router.post('/',authenticateToken, async (req:Request,res:Response) => {
+    const investments = await readData();
+    const newInvestment = req.body;
+    investments.push(newInvestment);
+    await writeData(investments);
+    res.status(201).json(newInvestment);
+});
+
+export default router;
