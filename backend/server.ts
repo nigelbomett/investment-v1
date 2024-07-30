096258b259aa5671eb12febf3426627028b25733
Nigel Bomett <nigelbomett@gmail.com>
2024-07-30 12:24:55 +0300

    registered routes

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import investmentRoutes from './routes/investments';
+import authRoutes from './routes/auth';
+
+
+
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/investments', investmentRoutes);
+app.use('/auth',authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
